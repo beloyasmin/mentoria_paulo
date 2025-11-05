9ecf3f111282028080659b4dbb712ec5aa08eb41
Yasmin Belo <beloyasmin93@gmail.com>
2025-11-05 20:07:32 -0300

    primeira aula

---
+++ lib/aula_01/variaveis.dart
@@ -0,0 +1,14 @@
+void main() {
+  const String nome = "Yasmin";
+  final int idade = 23;
+  bool maiorDeIdade = true;
+  double peso = 58.9;
+
+  final List<String> lista = [];
+  lista.add(nome);
+
+  print("Nome: $nome");
+  print("Idade: $idade");
+  print("Maior de idade: $maiorDeIdade");
+  print("Peso: $peso");
+}
